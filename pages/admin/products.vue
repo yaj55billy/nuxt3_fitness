fe9757e6bc6842c929ff1399cbf0820cd9be081b
yaj55billy <yaj55ushydna@gmail.com>
2023-08-22 21:31:28 +0800

    admin/products page(60%)

--- pages/admin/products.vue
+++ pages/admin/products.vue
@@ -2,6 +2,9 @@
 import { useLoading } from "vue-loading-overlay";
 import Pagination from "@/components/Pagination.vue";
 
+// bootstrap js
+const { $bootstrap } = useNuxtApp();
+
 // Loading
 const pageLoading = ref(null);
 
@@ -23,7 +26,12 @@ const products = ref([]);
 const pagination = ref({});
 const tempProduct = ref({ imageUrl: [] });
 const fileUpLoading = ref(false);
-const isLoading = ref(false);
+
+// 取 dom
+const productModal = ref(null);
+const delProductModal = ref(null);
+let productModalHandle;
+let delProductModalHandle;
 
 // about api
 const token = ref("");
@@ -40,6 +48,7 @@ let headers = {
 
 const getProducts = (num = 1) => {
 	const loader = $loading.show();
+
 	const api = `${config.public.apiUrl}/${config.public.uuid}/admin/ec/products`;
 	$fetch(api, {
 		method: "GET",
@@ -53,7 +62,9 @@ const getProducts = (num = 1) => {
 		.finally(() => {
 			loader.hide();
 		});
-
+	tempProduct.value = {
+		imageUrl: [],
+	};
 	// if (this.tempProduct.id) {
 	// 	this.tempProduct = {
 	// 		imageUrl: [],
@@ -65,10 +76,106 @@ const getProducts = (num = 1) => {
 	// }
 };
 
+const openModal = (status, item) => {
+	if (status === "new") {
+		tempProduct.value = {
+			imageUrl: [],
+		};
+		productModalHandle.show();
+	} else {
+		const loader = $loading.show();
+
+		const api = `${config.public.apiUrl}/${config.public.uuid}/admin/ec/product/${item.id}`;
+		$fetch(api, {
+			method: "GET",
+			headers: headers,
+		}).then((res) => {
+			tempProduct.value = res.data;
+			status === "edit"
+				? productModalHandle.show() // 編輯
+				: delProductModalHandle.show(); // 刪除
+			loader.hide();
+		});
+	}
+};
+
+const updateData = () => {
+	const loader = $loading.show();
+
+	let api = `${config.public.apiUrl}/${config.public.uuid}/admin/ec/product`,
+		httpMethod = "post",
+		successText = "產品新增成功",
+		failText = "產品新增失敗，請再檢查看看";
+
+	if (tempProduct.value.id) {
+		api = `${config.public.apiUrl}/${config.public.uuid}/admin/ec/product/${tempProduct.value.id}`;
+		httpMethod = "patch";
+		successText = "產品編輯成功";
+		failText = "產品編輯失敗，請再檢查看看";
+	}
+
+	$fetch(api, {
+		method: httpMethod,
+		headers: headers,
+		body: tempProduct.value,
+	})
+		.then((res) => {
+			productModalHandle.hide();
+			store.messageHandle(successText);
+			store.isShowHandle();
+			loader.hide();
+			getProducts();
+		})
+		.catch(() => {
+			loader.hide();
+			productModalHandle.hide();
+			store.messageHandle(failText);
+			store.isShowHandle();
+		});
+};
+
+// deleteProduct() {
+//       this.isLoading = true;
+//       const url = `${process.env.VUE_APP_APIPATH}/${process.env.VUE_APP_UUID}/admin/ec/product/${this.tempProduct.id}`;
+//       this.axios.delete(url).then(() => {
+//         $('#delProductModal').modal('hide');
+//         this.$bus.$emit('notice-user', '產品已刪除');
+//         this.isLoading = false;
+//         this.getProducts();
+//       }).catch(() => {
+//         this.isLoading = false;
+//         $('#delProductModal').modal('hide');
+//         this.$bus.$emit('notice-user', '產品刪除失敗，請再檢查看看');
+//       });
+//     }
+// uploadFile() { // 上傳圖片
+//   this.fileUpLoading = true;
+//   const catchFile = document.querySelector('#customFile').files[0];
+//   const formData = new FormData();
+//   formData.append('file', catchFile);
+//   const url = `${process.env.VUE_APP_APIPATH}/${process.env.VUE_APP_UUID}/admin/storage`;
+//   this.axios.post(url, formData, {
+//     header: {
+//       'Content-Type': 'multipart/form-data',
+//     },
+//   }).then((res) => {
+//     this.fileUpLoading = false;
+//     if (res.status === 200) {
+//       this.tempProduct.imageUrl.push(res.data.data.path);
+//     }
+//     document.querySelector('#customFile').value = '';
+//   })
+//     .catch(() => {
+//       this.$bus.$emit('notice-user', '檔案上傳失敗，請再檢查是不是檔案大小超過 2MB');
+//       this.fileUpLoading = false;
+//     });
+// },
+
 onMounted(() => {
 	getProducts();
-	// store.messageHandle("檔案上傳失敗，請再檢查是不是檔案大小超過 2MB");
-	// store.isShowHandle();
+
+	productModalHandle = new $bootstrap.Modal(productModal.value, {});
+	delProductModalHandle = new $bootstrap.Modal(delProductModal.value, {});
 });
 </script>
 
@@ -77,7 +184,7 @@ onMounted(() => {
 	<div class="mt-4">
 		<h2>產品列表</h2>
 		<div class="text-end mt-sm-2 mt-4">
-			<button class="btn btn-primary" @click="openModal('new')">
+			<button type="button" class="btn btn-primary" @click="openModal('new')">
 				建立新的產品
 			</button>
 		</div>
@@ -124,5 +231,219 @@ onMounted(() => {
 			</tbody>
 		</table>
 		<pagination :pagedata="pagination" @update="getProducts"></pagination>
+
+		<div
+			class="modal fade"
+			id="productModal"
+			ref="productModal"
+			tabindex="-1"
+			role="dialog"
+			aria-labelledby="exampleModalLabel"
+			aria-hidden="true"
+		>
+			<div class="modal-dialog modal-xl" role="document">
+				<div class="modal-content border-0">
+					<div class="modal-header bg-dark text-white">
+						<h5 id="exampleModalLabel" class="modal-title">
+							<span v-if="tempProduct.id">編輯 {{ tempProduct.title }}</span>
+							<span v-else>新增產品</span>
+						</h5>
+						<button
+							type="button"
+							class="btn-close white"
+							data-bs-dismiss="modal"
+							aria-label="Close"
+						></button>
+					</div>
+					<div class="modal-body text-left">
+						<div class="row">
+							<div class="col-sm-4">
+								<div v-for="item in 3" :key="item + 'img'" class="form-group">
+									<label :for="'imageUrl_' + (item - 1)">輸入圖片網址</label>
+									<input
+										:id="'imageUrl_' + (item - 1)"
+										v-model="tempProduct.imageUrl[item - 1]"
+										type="text"
+										class="form-control"
+										placeholder="請輸入圖片連結"
+									/>
+								</div>
+								<div class="form-group">
+									上傳圖片
+									<div
+										class="spinner-border text-primary"
+										role="status"
+										style="width: 12px; height: 12px"
+										v-if="fileUpLoading"
+									>
+										<span class="sr-only">Loading...</span>
+									</div>
+									<input
+										type="file"
+										class="form-control h-auto"
+										id="customFile"
+										@change="uploadFile"
+									/>
+								</div>
+								<div v-for="item in 3" :key="item + 'img-show'">
+									<img
+										class="img-fluid"
+										:src="tempProduct.imageUrl[item - 1]"
+										:alt="'課程示意: ' + tempProduct.title"
+									/>
+								</div>
+							</div>
+							<div class="col-sm-8">
+								<div class="form-group">
+									<label for="title">標題</label>
+									<input
+										id="title"
+										v-model="tempProduct.title"
+										type="text"
+										class="form-control"
+										placeholder="請輸入標題"
+									/>
+								</div>
+								<div class="form-row">
+									<div class="form-group col-md-6">
+										<label for="category">分類</label>
+										<input
+											id="category"
+											v-model="tempProduct.category"
+											type="text"
+											class="form-control"
+											placeholder="請輸入分類"
+										/>
+									</div>
+									<div class="form-group col-md-6">
+										<label for="price">單位</label>
+										<input
+											id="unit"
+											v-model="tempProduct.unit"
+											type="unit"
+											class="form-control"
+											placeholder="請輸入單位"
+										/>
+									</div>
+								</div>
+								<div class="form-row">
+									<div class="form-group col-md-6">
+										<label for="origin_price">原價</label>
+										<input
+											id="origin_price"
+											v-model="tempProduct.origin_price"
+											type="number"
+											class="form-control"
+											placeholder="請輸入原價"
+										/>
+									</div>
+									<div class="form-group col-md-6">
+										<label for="price">售價</label>
+										<input
+											id="price"
+											v-model="tempProduct.price"
+											type="number"
+											class="form-control"
+											placeholder="請輸入售價"
+										/>
+									</div>
+								</div>
+								<hr />
+								<div class="form-group">
+									<label for="description">產品描述</label>
+									<textarea
+										id="description"
+										v-model="tempProduct.description"
+										type="text"
+										class="form-control"
+										placeholder="請輸入產品描述"
+									>
+									</textarea>
+								</div>
+								<div class="form-group">
+									<label for="content">說明內容</label>
+									<textarea
+										id="description"
+										v-model="tempProduct.content"
+										type="text"
+										class="form-control"
+										placeholder="請輸入說明內容"
+									>
+									</textarea>
+								</div>
+								<div class="form-group">
+									<div class="form-check">
+										<input
+											id="enabled"
+											v-model="tempProduct.enabled"
+											class="form-check-input"
+											type="checkbox"
+										/>
+										<label class="form-check-label" for="enabled"
+											>是否啟用</label
+										>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="modal-footer">
+						<button
+							type="button"
+							class="btn btn-outline-secondary"
+							data-bs-dismiss="modal"
+						>
+							取消
+						</button>
+						<button type="button" class="btn btn-primary" @click="updateData">
+							確認
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div
+			id="delProductModal"
+			ref="delProductModal"
+			class="modal fade"
+			tabindex="-1"
+			role="dialog"
+			aria-labelledby="exampleModalLabel"
+			aria-hidden="true"
+		>
+			<div class="modal-dialog" role="document">
+				<div class="modal-content border-0">
+					<div class="modal-header bg-danger text-white">
+						<h5 id="exampleModalLabel" class="modal-title">
+							<span>刪除產品</span>
+						</h5>
+						<button
+							type="button"
+							class="btn-close btn-white"
+							data-bs-dismiss="modal"
+							aria-label="Close"
+						></button>
+					</div>
+					<div class="modal-body">
+						是否刪除
+						<strong class="text-danger">{{ tempProduct.title }}</strong>
+						商品(刪除後將無法恢復)。
+					</div>
+					<div class="modal-footer">
+						<button
+							type="button"
+							class="btn btn-outline-secondary"
+							data-bs-dismiss="modal"
+						>
+							取消
+						</button>
+						<button type="button" class="btn btn-danger" @click="deleteProduct">
+							確認刪除
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
